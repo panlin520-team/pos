75a8f20b86367a20f2488058b580f3e7e548ff02
ccqdoing <1084572119@qq.com>
2019-11-28 14:01:57 +0800

    初步编辑
    总体架构、公用组件、数值运算精确度、组件引入与使用

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="header"></div>
+    <div class="container">
+      <div class="aside"></div>
+      <div class="main"><router-view /></div>
     </div>
-    <router-view/>
+    <!-- <div class="footer"></div> -->
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
